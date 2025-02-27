81e726fa476666c7eee510c587e037aa32b364b8
bena-sudo <joanbenaventcastells@gmail.com>
2025-02-27 18:02:16 +0100

    add graphic

--- gastosub-app/src/app/components/dashboard/dashboard.component.ts
+++ gastosub-app/src/app/components/dashboard/dashboard.component.ts
@@ -3,10 +3,17 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { CurrencyEurPipe } from '../../pipes/currency-eur.pipe';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatToolbarModule, MatCardModule, MatListModule, CurrencyEurPipe],
+  imports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule,
+    CurrencyEurPipe,
+    NgxChartsModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
@@ -16,6 +23,17 @@ export class DashboardComponent {
     { name: 'Netflix', renewalDate: '10/03/2024' },
     { name: 'Spotify', renewalDate: '28/02/2024' },
     { name: 'Coursera', renewalDate: '05/03/2024' },
+  ];
+  chartData = [
+    {
+      name: 'Gasto',
+      series: [
+        { name: 'Enero', value: 150 },
+        { name: 'Febrero', value: 200 },
+        { name: 'Marzo', value: 120 },
+        { name: 'Abril', value: 180 },
+      ],
+    },
   ];
 
   constructor() {}
